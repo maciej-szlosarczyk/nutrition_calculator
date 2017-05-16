58460b58c78d46c43f7b98ad0256b97b0f004a8c
Maciej Szlosarczyk <maciej.szlosarczyk@inbank.ee>
2017-05-16 19:54:44 +0300

    Add conversion function

--- lib/person/weight.ex
+++ lib/person/weight.ex
@@ -3,6 +3,8 @@ defmodule NutritionCalculator.Weight do
   Functions and types to perform weight-related operations
   """
 
+  @lbs 2.20462
+
   @type weight :: {:kg, number()} | {:lbs, number()}
 
   @doc """
@@ -28,5 +30,24 @@ defmodule NutritionCalculator.Weight do
 
   def create_weight(_value, _unit) do
     raise(ArgumentError, message: "Unknown value type")
+  end
+
+  @spec convert_weight(weight, :kg) :: weight
+  def convert_weight(weight, :kg) do
+    {:lbs, number} = weight
+    {:kg, (number / @lbs)}
+  end
+
+  @spec convert_weight(weight, :lbs) :: weight
+  def convert_weight(weight, :lbs) do
+    {:kg, number} = weight
+    {:lbs, (number * @lbs)}
+  end
+
+  @doc """
+  Throw an error when arguments do not fit the function.
+  """
+  def convert_weight(_weight, _measure) do
+    raise(ArgumentError, message: "Wrong types for conversion")
   end
 end
--- test/person/weight_test.exs
+++ test/person/weight_test.exs
@@ -6,48 +6,61 @@ defmodule WeightTest do
   doctest NutritionCalculator.Weight
 
   describe "create_weight/1" do
-    test "with numeric argument" do
+    test "return value with numeric argument" do
       weight = Weight.create_weight(12)
 
       assert weight == {:kg, 12}
     end
-  end
-  defmodule WeightTest do
-    use ExUnit.Case, async: true
 
-    alias NutritionCalculator.Weight
-
-    doctest NutritionCalculator.Weight
-
-    describe "create_weight/1" do
-      test "return value with numeric argument" do
-        weight = Weight.create_weight(12)
-
-        assert weight == {:kg, 12}
-      end
-
-      test "raise error with any other argument" do
-        assert_raise ArgumentError, fn ->
-          Weight.create_weight(:atom)
-        end
+    test "raise error with any other argument" do
+      assert_raise ArgumentError, fn ->
+        Weight.create_weight(:atom)
       end
     end
+  end
 
-    describe "create_weight/2" do
-      test "return appropriate value with :kg or :lbs" do
-        weight = Weight.create_weight(12, :kg)
+  describe "create_weight/2" do
+    test "return appropriate value with :kg or :lbs and a number" do
+      weight = Weight.create_weight(12, :kg)
 
-        assert weight == {:kg, 12}
+      assert weight == {:kg, 12}
+    end
+
+    test "raise error with other type of arguments" do
+      assert_raise ArgumentError, fn ->
+        Weight.create_weight(12, :atom)
       end
 
-      test "raise error with other type of argument" do
-        assert_raise ArgumentError, fn ->
-          Weight.create_weight(12, :atom)
-        end
+      assert_raise ArgumentError, fn ->
+        Weight.create_weight("Not a number", :kg)
+      end
+    end
+  end
 
-        assert_raise ArgumentError, fn ->
-          Weight.create_weight("Not a number", :kg)
-        end
+  describe "convert_weight/2" do
+    test "converts from kgs to lbs" do
+      weight = Weight.create_weight(100, :kg)
+      converted_weight = Weight.convert_weight(weight, :lbs)
+
+      assert converted_weight == {:lbs, 220.462}
+    end
+
+    test "convert from lbs to kg" do
+      weight = Weight.create_weight(100, :lbs)
+      converted_weight = Weight.convert_weight(weight, :kg)
+
+      assert converted_weight == {:kg, 45.35929094356398}
+    end
+
+    test "raise error when arguments are wrong" do
+      assert_raise ArgumentError, fn ->
+        Weight.convert_weight(:atom, :atom)
+      end
+
+      weight = Weight.create_weight(100)
+
+      assert_raise ArgumentError, fn ->
+        Weight.convert_weight(weight, 100)
       end
     end
   end
